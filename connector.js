987cc5f6ced0a41c45fa2fe50ca89ee03b43aef4
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-10-28 14:16:41 -0400

    axios installed variable to hold shopIDs list Used express to set up 2 end points: 1./shopIds : fecths and feeds shopIDs array with 25 shopIDs numbers 2./shopListing : returns the listing information for each shopID on the ShopIDs array.

--- connector.js
+++ connector.js
@@ -1,4 +1,10 @@
-require('dotenv').config()
+/*require('dotenv').config()
 
 const apiKey = process.env.API_KEY;
 
+var shopListing = get('https://openapi.etsy.com/v2/shops?callback=findAllShopListingsActive&api_key=nhuh42k03xoiq9t8e67peg4u', (req,res)=>{
+    console.log(res)
+})
+
+module.exports = shopListing;
+*/
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require('express')
+require('dotenv').config()
+const axios = require('axios')
+
+const apiKey = process.env.API_KEY;
 const app = express()
 const port = 3000
 
-app.get('/', function(req, res) {
-    res.send('Hello world')
+//get list of 25 random shopIds
+let shopIDs = [];
+app.get('/shopIds', (req, res) => {
+   var url = `https://openapi.etsy.com/v2/shops?callback=findAllShopListingsActive&api_key=${apiKey}`
+
+   axios.get(url)
+   .then(data => {
+       let list = data.data.results;
+       for (let i = 0; i < list.length; i++) {
+           shopIDs.push(list[i].shop_id)
+       }
+       res.send(shopIDs)
+   })
+   .catch(err => res.send(err));
+});
+
+//get listings of 25 random shopIds
+app.get('/shopListing', (req, res) => {
+    let url = `https://openapi.etsy.com/v2/shops/:shop_id/listings/active?api_key=${apiKey}`
+    for (let i = 0; i < shopIDs.length; i++) {
+        let shop = shopIDs[i];
+        let myParams = {'shop_id': shop};
+    
+        axios.get(url, {
+            params: myParams
+        })                      
+        .then(data=>res.send(data.data))
+        .catch(err=>res.send(err))
+    }
 })
+
+
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
