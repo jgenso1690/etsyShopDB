735936c2d518435697add39fddadf6b5ad2b842b
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-10-28 21:27:30 -0400

    checking if a file for that shop exists if it doesnt it is creating a new file with the correspoding listing information as a stringify object it is getting the information from the etsy api for that specific shop I should abstract this axios get method because I will be using it to grab the current information for the shop in the next step

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 const axios = require('axios')
 
@@ -14,7 +16,7 @@ app.get('/shopIds', (req, res) => {
    axios.get(url)
    .then(data => {
        let list = data.data.results;
-       for (let i = 0; i < list.length; i++) {
+       for (let i = 0; i < 5; i++) {
            shopIDs.push(list[i].shop_id)
        }
        res.send(shopIDs)
@@ -24,19 +26,44 @@ app.get('/shopIds', (req, res) => {
 
 //get listings of 25 random shopIds
 app.get('/shopListing', (req, res) => {
-    let url = `https://openapi.etsy.com/v2/shops/:shop_id/listings/active?api_key=${apiKey}`
+    let url = `https://openapi.etsy.com/v2/shops/:shop_id/listings/active?api_key=${apiKey}`;
+
     for (let i = 0; i < shopIDs.length; i++) {
         let shop = shopIDs[i];
         let myParams = {'shop_id': shop};
-    
-        axios.get(url, {
-            params: myParams
-        })                      
-        .then(data=>res.send(data.data))
-        .catch(err=>res.send(err))
+
+        //check if there is a file for that shop
+        fs.readFile(`./shopsListings/${shop}`, 'utf8',  (err, data) => {
+            //if no file, fetch for creating a new file
+            if (err) {
+                axios.get(url, {
+                params: myParams
+                })                      
+                .then(data=>{
+                    let shopInfo = data.data
+                    let listings = shopInfo.results
+                    //console.log('shop: ',shop, 'listings: ', listings)
+                    fs.appendFile(`shopsListings/${shop}.txt`, `${JSON.stringify(listings)}`, function(err) {
+                        if (err) throw err;
+                        console.log('file created!')
+                    })
+                    res.end()
+                })
+                .catch(err=>res.send(err))
+            } else {
+                //if yes, fetch for current info
+                //search for differences in files(items added and items removed)
+                axios.get(url, {
+                params: myParams
+                })                      
+                .then(data=>{
+                    console.log(data.data)    
+                    res.send('check this file')
+                })
+                .catch(err=>res.send(err))
+            };
+        })            
     }
 })
-
-
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
